7e8ec736a1d6b2b765a6eb3bad7be8b625d57f8c
Trevor Yao <trevoryao@gmail.com>
2021-05-31 14:01:20 -0400

    multithreaded output!

--- src/problem.cc
+++ src/problem.cc
@@ -23,49 +23,19 @@ int Problem::solve(ostream &os) {
     // vector<thread> threads;
     // vector<promise<int>> promises;
     vector<future<int>> futures;
-    vector<stringstream> sss;
 
     for (int k = 1; k <= numbers.size(); ++k) {
-        stringstream ss;
-        sss.emplace_back(move(ss));
-        futures.emplace_back(async([this, &ss, k] {
-            return solve_k(ss, k);
+        futures.emplace_back(async([this, &os, k] {
+            stringstream ss;
+            int solved = solve_k(ss, k);
+            os << ss.str();
+            return solved;
         }));
     }
 
     int solved = 0;
     for (auto &f : futures) solved += f.get();
-    for (auto &ss : sss) os << ss.str();
     return solved;
-
-    // WORKING
-    /*
-    int solved = 0;
-    stringstream ss;
-    
-    for (int k = 1; k <= numbers.size(); ++k) {
-        do {
-            // something with first k numbers
-            // generate k - 1 operators
-            Generator gen{ops, static_cast<size_t>(k - 1)};
-            do {
-                try {
-                    Expression exp{numbers.begin(), numbers.begin() + k,
-                        gen.get_word().begin(), gen.get_word().end()};
-
-                    if (exp.is_solution(target)) {
-                        if (config.pretty_print) exp.output_pretty(os);
-                        else exp.output_json(os);
-                        ++solved;
-                    }
-                }
-                catch (ExpressionDivException) { }
-            } while (gen.next_word());
-        } while (next_k_permutation(numbers.begin(), numbers.end(), k));
-    }
-
-    // os << ss.str();
-    return solved;*/
 }
 
 int Problem::solve_k(ostream &os, int k) {
